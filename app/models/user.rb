75443af61fd25fcc2692ecd946e4e267a0b7e910
talow808 <808bobbrand@gmail.com>
2023-07-31 15:34:25 +0900

    確認

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,18 @@ class User < ApplicationRecord
   validates :family_name, presence: true
   validates :k_family_name, presence: true
   validates :k_first_name, presence: true
-  validates :encrypted_password, presence: true, length: { minimum: 6 }, format: { with: /\A[a-zA-Z0-9]+\z/ }
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable   
 
+  validates :password, presence: true, length: { minimum: 6 }
+  validate :password_complexity
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  private
+
+  def password_complexity
+    return if password.blank? || password.match?(/\A(?=.*[a-zA-Z])(?=.*\d)/)
+
+    errors.add(:password, 'は英字と数字の両方を含む必要があります')
+  end
 end
